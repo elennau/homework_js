647251bf8552fbc3b24e2d49010a81f2d5187284
Olena Naumets <elennau7@gmail.com>
2016-04-04 19:32:17 +0300

    JavaScript DZ5-6 and DZ7-8

---
+++ DZ5-6/js/script.js
@@ -0,0 +1,76 @@
+﻿'use strict'
+var myTimer = {
+proceedTimerID:'',
+prevTime : 0,
+currTime : 0,
+startTime : 0,
+startTimeID : 0,
+element:'', 
+
+proceed: function() {
+	myTimer.proceedTimerID = setInterval (function() {
+		var t = myTimer.prevTime + myTimer.currTime - myTimer.startTime;
+		var ms = t % 1000;
+		t = (t / 1000) >> 0;
+		var s = t % 60;
+		t = (t / 60) >> 0;
+		var m = t % 60;
+		t = (t / 60) >> 0;
+		var h = t % 24;
+		var timerBig = '' + ((h / 10) >> 0) + (h % 10) + ':' +
+							   ((m / 10) >> 0) + (m % 10) + ':' +
+							   ((s / 10) >> 0) + (s % 10); 
+		var timerSec = '' + ((ms / 100) >> 0) + ((ms % 100/10) >> 0) + (ms % 10);
+		document.TestForm.BigClock.value=timerBig; 
+		document.TestForm.SecClock.value=timerSec; 
+	}, 1);
+},
+
+
+startClock: function() { 
+	var t = new Date();
+	myTimer.startTime = t.getTime();
+	myTimer.currTime = myTimer.startTime;
+	myTimer.startTimeID = setInterval (function(){
+		var t = new Date();
+		myTimer.currTime = t.getTime();
+	}, 1);
+	myTimer.showStart();
+}, 
+
+stopClock: function() {
+	clearInterval(myTimer.startTimeID);
+	var t = new Date();
+	myTimer.prevTime = myTimer.prevTime + myTimer.currTime - myTimer.startTime;
+	myTimer.currTime = 0;
+	myTimer.startTime = 0;
+	myTimer.showStop();
+},
+
+clearClock: function() { 
+	clearInterval(myTimer.startTimeID);
+	myTimer.prevTime = 0;
+	myTimer.currTime = 0;
+	myTimer.startTime = 0;
+	myTimer.showStop();
+},
+
+showStart: function () {
+	myTimer.element = document.querySelector('.clock__start');	
+	myTimer.element.classList.add('show');
+	myTimer.element = document.querySelector('.clock__stop');	
+	myTimer.element.classList.remove('show');
+},
+
+showStop: function () {
+	myTimer.element = document.querySelector('.clock__stop');	
+	myTimer.element.classList.add('show');
+	myTimer.element = document.querySelector('.clock__start');	
+	myTimer.element.classList.remove('show');
+}
+}
+
+window.addEventListener('click', myTimer.proceed);
+document.TestForm.StartClock.addEventListener('click', myTimer.startClock);
+document.TestForm.StopClock.addEventListener('click', myTimer.stopClock);
+document.TestForm.ClearClock.addEventListener('click', myTimer.clearClock);
---
+++ DZ7-8/js/script.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function(){
+    $(".form").hover(function(){
+        $(this).next('span.help').toggleClass("show");
+    });
+});
+
+$(document).ready(function() {
+	$('ul.tabs__nav').on('click', 'li:not(.active)', function() {
+		$(this)
+			.addClass('active').siblings().removeClass('active')
+			.closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+	});
+
+});
+
