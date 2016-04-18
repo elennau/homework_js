716b8ba1aaf8e4aa03e6150808147a99bd71d284
Olena Naumets <elennau7@gmail.com>
2016-04-18 14:31:23 +0300

    JavaScript DZ11-12 new

--- DZ11-12/Micro-Tmpl/js/script.js
+++ DZ11-12/Micro-Tmpl/js/script.js
@@ -1,29 +1,37 @@
-﻿$(function() {
+﻿'use strict';
+$(function() {
 	var headHtml = $("#head").html(); 
 	var listHtml = $("#list").html(); 
 	var footHtml = $("#footer").html(); 
-	var myHead = [
-	'Наумец Елена Викторовна',
-	'img/photo.jpg',
-	'Главный специалист отдела IT'
-	];
 	
+	var myHead = {
+	name: 'Наумец Елена Викторовна',
+	img: 'img/photo.jpg',
+	work: 'Главный специалист отдела IT'
+	};
+
 	var myList = [
 	{item: 'мне надоела моя скучная работа'},
 	{item: 'у меня мизерная зарплата'},
 	{item: 'я хочу делать что-то новое и интересное'}
 	];
+
+	var myFoot = {
+	tel: '+380958031107',
+	href: 'https://www.facebook.com/elennau7/',
+	prof: 'Facebook',
+	feedback: 'Меньше слов, а больше дела!'
+	};
 		
-	var myFoot = [
-	'+380958031107',
-	'https://www.facebook.com/elennau7/',
-	'Facebook',
-	'Меньше слов, а больше дела!'
-	];
-	
-	var content1 = tmpl(headHtml, {data: myHead});
-	var content2 = tmpl(listHtml, {data1: myList});
-	var content3 = tmpl(footHtml, {data2: myFoot});
+	var content1 = tmpl(headHtml, { 
+	dataHead: myHead  
+	});
+	var content2 = tmpl(listHtml, { 
+	dataList: myList  
+	});
+	var content3 = tmpl(footHtml, { 
+	dataFoot: myFoot  
+	});
 	
 	$('body').append(content1);
 	$('.box1').append(content2);
