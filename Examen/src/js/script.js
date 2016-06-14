0f99a7ba0f44fbed35cad4aa92d742696f7861dd
elennau <elennau7@gmail.com>
2016-06-14 20:40:46 +0300

    examen

---
+++ Examen/src/js/script.js
@@ -0,0 +1,57 @@
+﻿'use strict;'
+$(function(){
+	$('.jcarousel')
+    .jcarousel({
+      animation: 'slow',
+      wrap: 'circular'
+     })
+    .jcarouselAutoscroll({
+      interval: 5000,
+      target: '+=1',
+      autostart: true
+    });
+	$('.jcarousel-control-prev').jcarouselControl({
+      target: '-=1'
+	});
+
+	$('.jcarousel-control-next').jcarouselControl({
+      target: '+=1'
+	});
+	
+	$('.grid').masonry({
+		itemSelector: '.grid__item'
+	});
+
+			
+	$('form').on('submit', function(e){
+		e.preventDefault();
+		e.stopPropagation();
+		var word = $('.search__input').val() || '';
+		searchImg(word);
+	});
+
+	
+	function createTmpl(array){
+	var Html = $('#masonry').html(); 
+	var content = tmpl(Html, {data: array});
+    $('#ideas__grid').html(content);
+	};
+
+
+	var keyP = '2737229-d77c828bc96e9964645663545';
+
+	function searchImg(word){
+		
+	$.getJSON('http://pixabay.com/api/?key=' + keyP + '&q=' + word + '&image_type=photo&per_page=7',
+	function(data){
+		console.log(data);
+	    createTmpl(data.hits);
+		})
+	};
+
+	searchImg('holiday+weekend');
+
+});
+
+	
+
